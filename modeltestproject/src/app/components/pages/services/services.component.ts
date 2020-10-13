80288221dc7d942cc0614b6c29bda248c9b452b0
Your Name <you@example.com>
2020-10-13 15:46:49 -0700

    google picker connected google-drive

--- modeltestproject/src/app/components/pages/services/services.component.ts
+++ modeltestproject/src/app/components/pages/services/services.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import "gapi.auth2";
-//import "gapi";
 
 @Component({
   selector: 'app-services',
@@ -8,24 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
- 
-/*   public static file: gapi.client.drive.File;
-  public static fromGoogleFile(file: gapi.client.drive.File): ServicesComponent {
-  if(file){
-    this.file = file;
-    console.log("files from services" + this.file);
-    return this.file;
-  }
-   else {
-    throw new Error('Method not implemented.');
-   } 
-    
-  } */
+
+  oauthToken:any;
 
   constructor() { }
 
 /*   ngOnInit():void{
     ServicesComponent.fromGoogleFile(ServicesComponent.file);
   } */
+   loadPicker() {
+    gapi.load('auth', {'callback': this.onAuthApiLoad});
+    gapi.load('picker', {'callback': this.onPickerApiLoad});
+  }
+  
+   onAuthApiLoad() {
+    gapi.auth.authorize(
+        {
+          'client_id': "375973183467-h5njr2s69i4q4ph3l3hup55t3irf3rnu.apps.googleusercontent.com",
+          'scope': ['https://www.googleapis.com/auth/drive.file'],
+          'immediate': false
+        },
+        handleAuthResult);
+  }
+  
+   onPickerApiLoad() {
+    let pickerApiLoaded = true;
+    createPicker
+  }
+}
+
+function handleAuthResult(authResult){
+ let sv = new ServicesComponent();
  
+  if (authResult && !authResult.error) {
+   sv.oauthToken =  authResult.access_token;
+    createPicker(sv.oauthToken)
+  }
+}
+function createPicker(storedOauth){
+    let pickerApiLoaded = true;
+    console.log("value of OauthToken " + storedOauth)
+
+    if (pickerApiLoaded && storedOauth) {
+    var picker = new google.picker.PickerBuilder()
+        .enableFeature(google.picker.Feature.NAV_HIDDEN)
+        .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+        .setAppId("375973183467")
+        .setOAuthToken(storedOauth)
+        .addView(google.picker.ViewId.DOCS)
+        .addView(google.picker.ViewId.IMAGE_SEARCH)
+        .addView(google.picker.ViewId.PRESENTATIONS)
+        //.addView(new google.picker.DocsUploadView())
+        .setDeveloperKey("AIzaSyBbTMmvECP0SsdRErSZRf51YzWC3oDR5cM")
+        .setCallback(pickerCallback)
+        .build();
+     picker.setVisible(true);
+  }
+}
+function pickerCallback(data) {
+  if (data.action == google.picker.Action.PICKED) {
+    var fileId = data.docs[0].id;
+    alert('The user selected: ' + fileId);
+  }
 }
