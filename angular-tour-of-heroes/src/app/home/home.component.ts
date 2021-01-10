764e05557bc6ee8cc784c8f92238c456d3d62024
Your Name <you@example.com>
2021-01-09 18:21:22 -0800

    App css done

--- angular-tour-of-heroes/src/app/home/home.component.ts
+++ angular-tour-of-heroes/src/app/home/home.component.ts
@@ -10,7 +10,11 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.textToAppear = true;
+    },10000);
   }
+  textToAppear:boolean = false;
   readLocalStorageValue(key) {
     return localStorage.getItem(key)
   }
