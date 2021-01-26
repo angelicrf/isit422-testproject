dc5fb6b659d58fa9ba4b142b4d8d423f58946f6d
Your Name <you@example.com>
2021-01-26 14:43:40 -0800

    dp-upload largeFile

--- multicloudApp/routes/index.js
+++ multicloudApp/routes/index.js
@@ -1171,13 +1171,15 @@ router.get('/GDUpdateLocalFile' , (req, res) => {
             }
             else{
               console.log("inside the largeFiles ");
-              
-              await dpUploadSessionStart(saveAccess,concatFile);              
+              await dpSdkUpload(saveAccess,concatFile);
+              toDeleteAllFiles();
+              res.status(200).send("Response from Node: large local file uploaded to Dropbox") ;
+              /* await dpUploadSessionStart(saveAccess,concatFile);              
               let calcFileActualSize = await getFileSize(concatFile);
               await dpUploadLargeFileAppend(saveAccess,holdDpSessionId.toString(),concatFile,calcFileActualSize);
               await dpUploadSessionFinish(saveAccess,holdDpSessionId.toString(),concatFile,calcFileActualSize);
               toDeleteAllFiles();  
-              res.status(200).send("Response from Node: large file uploaded to Dropbox"); 
+              res.status(200).send("Response from Node: large file uploaded to Dropbox");  */
             }
         },35000);    
       }
@@ -1238,8 +1240,10 @@ router.post('/DPUploadLocal', (req, res) =>
               });
             }else{
               console.log("inside the largeFiles ");
-              dpSdkUpload(saveAccess,concatFile);
-             /*  await dpUploadSessionStart(saveAccess,concatFile);
+              await dpSdkUpload(saveAccess,concatFile);
+              toDeleteAllFiles();
+              res.status(200).send("Response from Node: large local file uploaded to Dropbox") ;
+              /*  await dpUploadSessionStart(saveAccess,concatFile);
               let calcFileActualSize = await getFileSize(concatFile);
               await dpUploadLargeFileAppend(saveAccess,holdDpSessionId.toString(),concatFile,calcFileActualSize);
               await dpUploadSessionFinish(saveAccess,holdDpSessionId.toString(),concatFile,calcFileActualSize);
@@ -1667,8 +1671,11 @@ router.post('/BxUpload', (req,res) => {
             }); 
           }else{
             console.log("inside large file bxUpload");
-            
             let bxActualFileSize = await getFileSize(boxConcatFile);
+            await bxSdkUpload(boxAccessToken,boxConcatFile,bxActualFileSize);
+            res.status(200).json({"bxLargeUploadMSG": "bxFile_Uploaded", "bxLargeFileUpload": "bxLargeFileUploadComleted"});
+            toDeleteAllFiles();
+            /* 
             await bxUploadSessionStart(boxAccessToken,bxActualFileSize,boxConcatFile);
             await encodeShaOne(part_size,boxConcatFile);
             await bxNewFlSizes(bxNewFiles);
@@ -1685,7 +1692,7 @@ router.post('/BxUpload', (req,res) => {
             bxFirstExec = false;
             await bxCommitSession(boxAccessToken,bxUpFileId,bxParts,bxCommitHash,bxFirstExec);
             res.status(200).json({"bxLargeUploadMSG": "bxFile_Uploaded", "bxLargeFileUpload": "bxLargeFileUploadComleted"});
-            toDeleteAllFiles(); 
+            toDeleteAllFiles(); */ 
           }
       },55000);
     }
@@ -2868,64 +2875,66 @@ async function bxSdkUpload(bxAccToken,flName,bxFileSize){
     });
   });
 }
-function dpSdkUpload(dpAccToken,fileName){
-  //dpAccToken
-  console.log("new AccessToken dp " + dpAccToken);
-  let dbx = new Dropbox({ accessToken: `${dpAccToken}` });
-  //console.log(dbx);
-  let filePath = './routes/AllFiles'
-  const maxBlob = 8 * 1000 * 1000; // 8Mb - Dropbox JavaScript API suggested max file / chunk size
-        var workItems = [];      
-        var offset = 0;
+async function dpSdkUpload(dpAccToken,fileName){
+ return await new Promise((resolve,reject) => {
+
+   let dbx = new Dropbox({ accessToken: `${dpAccToken.substr(1,dpAccToken.length-3)}` });
+   let filePath = './routes/AllFiles'
+   const maxBlob = 8 * 1000 * 1000; 
+        let workItems = [];      
+        let offset = 0;
         let dpSid = '';
-        //let fileName = 'testImage.jpg'
-        
-        var file = fs.readFileSync(filePath + '/testImage.jpg');
+        let file = fs.readFileSync(filePath + '/testImage.jpg');
         let fileSize = Buffer.byteLength(file);
-        //console.log(buff)
-       // console.log(file.slice(103, 111))
+      
                while (offset < fileSize) {
-                var chunkSize = Math.min(maxBlob, fileSize - offset);
+                let chunkSize = Math.min(maxBlob, fileSize - offset);
                 workItems.push(file.slice(offset, offset + chunkSize));
                 offset += chunkSize;
               }       
               const task = workItems.reduce((acc, blob, idx, items) => {
-                //console.log("acc " + acc + "blob " + blob + "idx " + idx + "items " +items)
                 if (idx == 0) {
-                  // Starting multipart upload of file
-                  return acc.then(function() {
-                    console.log('here1')
+                  return acc.then(() => { 
                     return dbx.filesUploadSessionStart({ close: false, contents: blob})
                               .then(response => {
-                                dpSid = JSON.stringify(dpSid.result.session_id);
+                                dpSid = JSON.stringify(response.result.session_id);
                                 console.log("dpSid " + dpSid)
-                                response.result.session_id;
+                                JSON.stringify(response.result.session_id);
                               })
                               .catch(err => console.log(err))
                   });          
                 } else if (idx < items.length-1) {  
-                  // Append part to the upload session
-                  return acc.then(function(sessionId) {
-                    console.log('here2')
+                  return acc.then((sessionId) => {  
                     sessionId = dpSid;
-                   var cursor = { session_id: sessionId, offset: idx * maxBlob };
-                   return dbx.filesUploadSessionAppendV2({ cursor: cursor, close: false, contents: blob })
-                   .then(() => sessionId)
-                   .catch(err => console.log(err)) 
+                    console.log("idx * maxBlob is " + idx * maxBlob)
+                    let cursor = { session_id: sessionId.substr(1,sessionId.length-2), offset: idx * maxBlob };
+                  
+                    return dbx.filesUploadSessionAppendV2({ cursor: cursor, close: false, contents: blob })
+                   .then((res) => {
+                    console.log(JSON.stringify(res));
+                    })
+                     .then(() => {
+                      console.log('done part one');
+                    }) 
+                   .catch(err => console.log(err)); 
                   });
                 } else {
-                  // Last chunk of data, close session
-                  return acc.then(function(sessionId) {
-                    console.log('here3')
+                  return acc.then((sessionId) => {
                     sessionId = dpSid;
-                    var cursor = { session_id: sessionId, offset: fileSize - blob.size };
-                    var commit = { path: '/' + fileName, mode: 'add', autorename: true, mute: false };              
-                    return dbx.filesUploadSessionFinish({ cursor: cursor, commit: commit, contents: blob });           
+                    let cursor = { session_id: sessionId.substr(1,sessionId.length-2), offset: fileSize - blob.byteLength }; 
+                    let commit = { path: '/MTC' + fileName , mode: 'add', autorename: true, mute: false , strict_conflict: false};              
+                   
+                    return dbx.filesUploadSessionFinish({ cursor: cursor, commit: commit, contents: blob })
+                    .then(() => console.log('here8'))
+                    .then(() => resolve("large file uploaded"))
+                    .catch(err => {
+                      console.log(JSON.stringify(err));
+                      reject(err);
+                     });           
                   });
                 }          
-              }, Promise.resolve());  
-         
+              }, Promise.resolve());
+      });    
 }
-dpSdkUpload('6LLEn6p22nsAAAAAAAAAASt50g1OEyvhg_lHog2RCGPs-Pq9Ayt5KhmwcbznkGNM','testImage.jpg');
 
 module.exports = router;
