2d87a66e5fe8aaca86346ca7cbae0632d89a8054
Your Name <you@example.com>
2021-01-29 12:42:34 -0800

    public dist to root

--- routes/index.js
+++ routes/index.js
@@ -79,7 +79,7 @@ if (process.env.NODE_ENV === 'production') {
 })
 }
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/dist/angular-multiclouds/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/angular-multiclouds/index.html'));
 });
 //app.use('/', indexapp);
 
