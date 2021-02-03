96b4ca80f588dc800ca32b507063e83d460967e4
Your Name <you@example.com>
2021-02-03 12:32:51 -0800

    prd mde routes

--- routes/index.js
+++ routes/index.js
@@ -64,14 +64,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-/* app.use(
+ app.use(
   express.static(path.join(__dirname, '../routes/dist/angular-multiclouds')),
 );
 app.get('*', function (req, res) {
   res.sendFile(
     path.join(__dirname, '../routes/dist/angular-multiclouds', 'index.html'),
   );
-}); */
+}); 
 
 let corsOptions = {
   origin: 'https://stormy-headland-33273.herokuapp.com',
@@ -81,7 +81,7 @@ app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === 'production') {
   console.log('app in production mode ....');
-  app.use(express.static('/dist/angular-multiclouds'));
+  app.use(express.static(__dirname, '/dist/angular-multiclouds'));
   app.get('*/', function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/angular-multiclouds/index.html'));
   });
